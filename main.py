364ee21ece9d72caf100733664dd0832a93bb031
Lier0102 <minehammer26@gmail.com>
2022-09-17 10:38:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ class Background:
     def __init__(self, screen): # initializer
         self.screen = screen # initialize screen object
         self.back_img = pygame.image.load("background.png") # load the background image
-    
+        self.back_img2 = pygame.image.load("background.png") # 2
+
+        self.x1 = 0
+        self.y1 = 0
+
+        self.x2 = 0
+        self.y2 = -960
+
     def draw(self): # draw the background image onto the screen
-        self.screen.blit(self.back_img, (0,0))
+        self.screen.blit(self.back_img, (self.x1,self.y1)) # scroll the background image onto the screen
+        self.screen.blit(self.back_img2, (self.x2,self.y2))
 
 def main():
     bg = Background(screen)
