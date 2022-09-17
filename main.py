4e5b0b9f6f1a1299cb37e1f64f04f83a8ac5275f
Lier0102 <minehammer26@gmail.com>
2022-09-17 10:34:04 +0900

    주석 일정 수정

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ screen = pygame.display.set_mode((640, 960)) # set window resolution to 640, 960
 pygame.display.set_caption("Juwon Game") # set the caption "Juwon Game"
 
 class Background:
-    def __init__(self, screen):
-        self.screen = screen
+    def __init__(self, screen): # initializer
+        self.screen = screen # initialize screen object
         self.back_img = pygame.image.load("background.png") # load the background image
     
-    def draw(self):
-        self.screen.blit(self.back_img, (0,0)) # draw the background image onto the screen
+    def draw(self): # draw the background image onto the screen
+        self.screen.blit(self.back_img, (0,0))
 
 def main():
     bg = Background(screen)
-    while True:
-        bg.draw()
+    while True: # main game loop
+        bg.draw() # draw the background image onto the screen
         pygame.display.update()
 
-main()
+main() # start the game
